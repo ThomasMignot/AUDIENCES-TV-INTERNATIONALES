1dee6de7e3606f62c0c91ae7979c91c988e1da57
ThomasMignot <t.mignot@endemolfrance.com>
2026-04-24 16:01:19 +0200

    Update nl_televizier.py

--- scrapers/nl_televizier.py
+++ scrapers/nl_televizier.py
@@ -54,7 +54,11 @@ HEADERS = {
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
     ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "nl-NL,nl;q=0.9,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 
 MONTHS_NL = {
